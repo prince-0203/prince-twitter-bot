04c5e1bb206e6064aa9ee576f4efac46f1f3179a
prince <mc.prince.0203@gmail.com>
2016-07-25 17:38:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const ipAddress = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP,
-      port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+const ipAddress = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP;
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 if (!ipAddress) {
   console.warn('No OPENSHIFT_*_IP var, using 127.0.0.1');
